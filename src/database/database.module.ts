b77d7ecb08d0177e59904dfe9d36a6e6266ce557
joaovicttor19 <victorjjoao@gmail.com>
2025-12-04 10:33:36 -0300

    chore: configure Sequelize and database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      models: [],
+      autoLoadModels: true,
+      synchronize: true,
+      logging: false,
+    }),
+  ],
+})
+export class DatabaseModule {}
